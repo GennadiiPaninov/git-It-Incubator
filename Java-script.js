b7e4b2134eb270398077f41dc5f47ee30361ab5f
GennadiiPaninov <utyf185@mail.ru>
2023-02-24 21:40:50 +0300

    Ceil

--- Java-script.js
+++ Java-script.js
@@ -18,3 +18,8 @@ function countdown(n){
 
  countdown(5);
 
+// The first century spans from the year 1 up to and including the year 100, the second century - from the year 101 up to and including the year 200, etc.
+// округление вверх
+const century = year => Math.ceil(year/100)
+
+century(2357);

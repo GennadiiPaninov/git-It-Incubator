f32a9349f1044718637be288f4365e837f66bad1
GennadiiPaninov <utyf185@mail.ru>
2023-02-24 21:20:58 +0300

    charCodeAt(0)

--- Java-script.js
+++ Java-script.js
@@ -1,7 +1,20 @@
+//You'll be given a string, and have to return the sum of all characters as an int. The function should be able to handle all printable ASCII characters.
+const uniTotal = s => {
+    let r = 0;
+    for (let i=0; i< s.length;i++){
+       
+    r += s[i].charCodeAt(0)
+    }
+    console.log(r)
+    return r;
+  }
+  uniTotal("sssD");
+
 
 function countdown(n){
     console.log(n)
     return n < 1 ? [] : [n].concat(countdown(n - 1));
  }
 
- countdown(5)
+ countdown(5);
+

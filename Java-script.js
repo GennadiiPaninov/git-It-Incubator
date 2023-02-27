c1b550b4e08267739d4a4b803c83e16451a8272b
GennadiiPaninov <utyf185@mail.ru>
2023-02-27 22:39:15 +0300

    Деструктуризация

--- Java-script.js
+++ Java-script.js
@@ -146,3 +146,12 @@ return res
 console.log("Спред оператор расспаковывает массив ")
 const maximus = Math.max(...mixArr);// выводит максимольное значение массива
 console.log(maximus)
+console.log(" Деструктуризация объекта")
+const HIGH_TEMPERATURES = {
+  yesterday: 75,
+  today: 77,
+  tomorrow: 80
+};
+
+// Only change code below this line
+  const { today: highToday, tomorrow: highTomorrow } = HIGH_TEMPERATURES;

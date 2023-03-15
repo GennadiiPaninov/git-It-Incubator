7f379cfec86e1e414442af7bab95b50b053e5ff5
GennadiiPaninov <utyf185@mail.ru>
2023-03-15 20:53:39 +0300

    H

--- Java-script №2.js
+++ Java-script №2.js
@@ -5,10 +5,60 @@ const result1 = arrey1.reduce((el1, el2)=> {
   return el1 + el2
 }, 0)// ноль это значение, в которое будет el2
 console.log(result1)
+ 
 const arrey2 = [
   { id: '123', name: 'John' },
   { id: '345', name: 'Anna' },
   { id: '567', name: 'Kate' },
   { id: '789', name: 'Jane' }
 ]
-const result2 =
+
+arrey2.splice(1,1)
+
+const result2 = arrey2.reduce((el1, obj)=>{
+  
+  return{
+    
+    ...el1, [obj.id]: obj.name, // копирование оператором spread 
+  }
+
+}, {id: '123', name: 'John', })// замыкание на объекте
+console.log(result2)
+
+
+
+
+
+
+
+
+
+
+// The maximum sum value of ranges -- Challenge version
+const maxsumArrayEl=(arr,ranges)=>{
+
+  const sumFrom0toN =[];
+
+  for(let i=0;i<arr.length;i++){
+    sumFrom0toN[i] = (sumFrom0toN[i-1] || 0) + arr[i];// в круглые скобки взяли для разделения и использования Или
+    //[ 1, -1, 2, 6, 1, -3, 0, 2, 3 ]
+    // не используя push
+  }
+ let currentMaxResult = -Infinity;
+
+  for(let i = 0; i < ranges.length; i++){
+    const [start, end]= ranges[i]; // деструктуризация, создаем срозу две переменные
+    const sum = sumFrom0toN[end] - (sumFrom0toN[start -1] || 0) //текущая сумма
+    console.log(sum, start, end)
+    //[1,3] start end, 
+    if(sum > currentMaxResult){
+      currentMaxResult = sum
+    }
+  }
+
+  return currentMaxResult
+}
+
+console.log(maxsumArrayEl([1,-2,3,4,-5,-4,3,2,1], [[1,3],[0,4],[6,8]]))
+console.log(maxsumArrayEl([1,-2,3,4,-5,-4,3,2,1], [[1,3]]))
+console.log(maxsumArrayEl([1,-2,3,4,-5,-4,3,2,1], [[1,4],[2,5]]))

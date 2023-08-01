be47876dd1207952503117cf6a20684ee8369a6d
Gennadij Lobotskij <utyf185@mail.ru>
2023-08-01 22:06:01 +0300

    01/08/2023

--- Java-script №2.js
+++ Java-script №2.js
@@ -229,3 +229,10 @@ var capitals = function (word) {
   }
   return result
 };
+function binToDec(bin){ let dec = 0;
+  for (let i = 0; i < bin.length; i++) {
+    const bit = Number(bin[i]);
+    dec += bit * Math.pow(2, bin.length - i - 1);
+  }
+  return dec;
+}

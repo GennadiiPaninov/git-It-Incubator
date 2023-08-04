95ade8eb32782dc7b4ef7cb02c679fea516dad45
Gennadij Lobotskij <utyf185@mail.ru>
2023-08-04 19:58:30 +0300

    04/08/2023

--- Java-script №2.js
+++ Java-script №2.js
@@ -257,3 +257,12 @@ const maskify=cc=>{
     return masked + lastFour;
   }
 }
+function removeEveryOther(arr){
+  let result= []
+  for(let i =0; i<arr.length; i++){
+    if(i%2===0){
+      result.push(arr[i])
+    }
+  }
+  return result
+}

78492deac47f2e1e43b2c03229e696e24195e702
Gennadij Lobotskij <utyf185@mail.ru>
2023-08-24 10:11:21 +0300

    24/08/2023

--- Java-script №2.js
+++ Java-script №2.js
@@ -361,3 +361,25 @@ function getMean(arr, x, y) {
   return ((sum1 / x) + (sum2/y)) /2
 }
 const flip=(d,a)=>a.sort((b,c)=>d=="R"? b-c:c-b)
+
+function boredom(staff){
+  var map = {
+    accounts:1,
+    finance:2,
+    canteen:10,
+    regulation:3,
+    trading: 6,
+    change:6,
+    IS:8,
+    retail:5,
+    cleaning:4,
+    'pissing about':25
+  };
+
+  var score = Object.keys(staff).reduce(
+      function(a,b){
+        return a+map[staff[b]]
+      },0);
+
+  return score <= 80 ? 'kill me now': score < 100 && score > 80 ? 'i can handle this' : 'party time!!';
+}

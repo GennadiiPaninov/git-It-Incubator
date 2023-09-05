82ca2ac33e026400eb9a4206691bb95210472f9d
Gennadij Lobotskij <utyf185@mail.ru>
2023-09-05 10:07:28 +0300

    05/09/2023

--- Java-script №2.js
+++ Java-script №2.js
@@ -501,3 +501,16 @@ function grabDoll(dolls){
   }
   return bag;
 }
+function giveMeFive(obj){
+  const result = []
+  for(let key in obj){
+    if(key.length === 5){
+      result.push(key)
+
+    }
+    if(obj[key].length === 5){
+      result.push(obj[key])
+    }
+  }
+  return result
+}

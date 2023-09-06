49ace23dbb03152371b770362d2aa379cd8e3836
Gennadij Lobotskij <utyf185@mail.ru>
2023-09-06 20:59:10 +0300

    06/09/2023

--- Java-script №2.js
+++ Java-script №2.js
@@ -514,3 +514,10 @@ function giveMeFive(obj){
   }
   return result
 }
+function whatNumberIsIt(n){
+  return `Input number is ${n == Number.POSITIVE_INFINITY ? 'Number.POSITIVE_INFINITY':
+      n == Number.NEGATIVE_INFINITY ? 'Number.NEGATIVE_INFINITY':
+          n == Number.MAX_VALUE ? 'Number.MAX_VALUE':
+              n == Number.MIN_VALUE ? 'Number.MIN_VALUE':
+                  Number.isNaN(n) ? 'Number.NaN': n}`
+}

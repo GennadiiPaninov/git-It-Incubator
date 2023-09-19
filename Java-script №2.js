7bc3cc5654d5227a66f8284c19ea926ecdb9f659
Gennadij Lobotskij <utyf185@mail.ru>
2023-09-19 11:24:20 +0300

    19/09/2023

--- Java-script №2.js
+++ Java-script №2.js
@@ -601,3 +601,13 @@ function threeInOne(arr){
   }
   return resArr
 }
+function sortIt(arr){
+  return arr.slice().sort((a,b)=>{
+    let countA = arr.filter(v=>v===a).length
+    let countB = arr.filter(v=>v===b).length
+    if (countA===countB){ return b-a}
+    else if (countA>countB) {return 1}
+    else { return -1}
+  })
+
+}

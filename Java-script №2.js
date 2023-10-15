dca1ec6b44d7b031534af7afac9719e25d642aee
Gennadij Lobotskij <utyf185@mail.ru>
2023-10-15 15:10:58 +0300

    15/10/2023#2

--- Java-script №2.js
+++ Java-script №2.js
@@ -854,3 +854,20 @@ function baublesOnTree(baubles, branches){
 const binaryArrayToNumber = arr => {
   return  parseInt(arr.join(""), 2)
 };
+var mispelled = function(word1, word2){
+  if (word1.length<word2.length){
+    let temp=word1;
+    word1=word2;
+    word2=temp;
+  }
+  let count=0;
+  for (let i=0,j=0;i<word1.length;i++,j++){
+    if (word1[i]!==word2[j]){
+      count++
+      if (word1.length>word2.length){
+        i++
+      }
+    }
+  }
+  return count<2
+}

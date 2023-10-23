c82826f135378f3bfaf6f9f93dd7444e69765b1e
Gennadij Lobotskij <utyf185@mail.ru>
2023-10-23 11:44:32 +0300

    23/10/2023

--- Java-script №2.js
+++ Java-script №2.js
@@ -934,3 +934,12 @@ function spinAround(turns) {
     return Math.floor((left-right) / 4)
   }
 }
+function trouble(x, t){
+  for(let i=0; i<x.length; i++) {
+    if(x[i]+x[i+1]===t) {
+      x.splice(i+1,1);
+      i--;
+    }
+  }
+  return x;
+}

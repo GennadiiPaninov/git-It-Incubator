bd7f7f1f635a46d8aa6d14f8835b008e183de0c3
Gennadij Lobotskij <utyf185@mail.ru>
2023-11-27 19:17:09 +0300

    7 kyu Reducing Problems - Bug Fixing #8

--- Java-script №2.js
+++ Java-script №2.js
@@ -1118,3 +1118,8 @@ var seqlist = function(first,c,l){
   }
   return arr
 }
+function calculateTotal(team1, team2) {
+  var t1s = team1.reduce((t, c) => t += c, 0);
+  var t2s = team2.reduce((t, c, i) => t+=c,0);
+  return t1s > t2s;
+}

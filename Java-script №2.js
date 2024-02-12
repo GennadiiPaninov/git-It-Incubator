23e846303e95e965ba02985b31c56e2976f12635
Gennadij Lobotskij <utyf185@mail.ru>
2024-02-12 19:30:44 +0300

    7 kyu Order of weight

--- Java-script №2.js
+++ Java-script №2.js
@@ -1507,3 +1507,17 @@ function spacify(str) {
 function toNumberArray(stringarray){
   return stringarray.map(el=>Number(el))
 }
+function arrange(arr){
+  const toNum = (num) => {
+    if (num.indexOf("KG") !== -1) {
+      return Number(`${num.substring(0, num.length - 2)}000`);
+    } else if (num.indexOf("G") !== -1) {
+      return Number(`${num.substring(0, num.length - 1)}`);
+    } else {
+      return Number(`${num.substring(0, num.length - 1)}000000`);
+    }
+  };
+  return  arr.sort((a, b) => {
+    return toNum(a) - toNum(b);
+  });
+}

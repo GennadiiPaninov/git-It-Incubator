7feab21cdc003c2b115b25525b74f4a46a22e296
Gennadij Lobotskij <utyf185@mail.ru>
2024-03-27 20:51:24 +0300

    More clear

--- Java-script №2.js
+++ Java-script №2.js
@@ -2090,3 +2090,7 @@ function canIPlay(now,start,end) {
   }
   return now >= start && now < end
 }
+function canIPlay2(now,start,end) {
+  for(let i = start; i != end; i = (i + 1) % 24) if(i === now) return true
+  return false
+}

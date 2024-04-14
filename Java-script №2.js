11b09da33b11e419af14290e5a779e4bb0c7faf3
Gennadij Lobotskij <utyf185@mail.ru>
2024-04-14 16:09:43 +0300

    More clear

--- Java-script №2.js
+++ Java-script №2.js
@@ -2366,3 +2366,8 @@ function findOutlier(i){
     return el % 2 === 0
   })
 }
+function findOutlier(int){
+  var even = int.filter(a=>a%2==0);
+  var odd = int.filter(a=>a%2!==0);
+  return even.length==1? even[0] : odd[0];
+}

715785de155baf7cae44afc61504d37c6c26bc30
Gennadij Lobotskij <utyf185@mail.ru>
2024-04-15 12:36:01 +0300

    More clear

--- Java-script №2.js
+++ Java-script №2.js
@@ -2413,3 +2413,9 @@ function isValidWalk(walk) {
   }
   return false
 }
+function isValidWalk(walk) {
+  function count(val) {
+    return walk.filter(function(a){return a==val;}).length;
+  }
+  return walk.length==10 && count('n')==count('s') && count('w')==count('e');
+}

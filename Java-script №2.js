72a0618b1a4864803fac9d8779587f7627aef028
Gennadij Lobotskij <utyf185@mail.ru>
2024-04-21 17:53:38 +0300

    Find the missing letter

--- Java-script №2.js
+++ Java-script №2.js
@@ -2645,3 +2645,12 @@ function solution(str){
     }
   },[])
 }
+function findMissingLetter(array){
+  for (let i = 1; i < array.length; i++) {
+    const prevCharCode = array[i - 1].charCodeAt(0)
+    const currentCharCode = array[i].charCodeAt(0)
+    if (currentCharCode - prevCharCode !== 1) {
+      return String.fromCharCode(prevCharCode + 1)
+    }
+  }
+}

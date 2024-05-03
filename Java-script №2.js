a6051336f3aaea5d8e78d69a81198855d0c3427e
Gennadij Lobotskij <utyf185@mail.ru>
2024-05-03 19:30:36 +0300

    Backspaces in string

--- Java-script №2.js
+++ Java-script №2.js
@@ -2838,3 +2838,14 @@ function wave(str){
     }
     return bytes;
   };
+  function clean_string(s) {
+  const result = []
+	for (const c of s) {
+    if (c === "#") {
+      result.pop()
+    } else {
+      result.push(c)
+    }
+  }
+  return result.join("")
+}

fb7263f20edc89daa3244b389fe9bb2d9029a731
Gennadij Lobotskij <utyf185@mail.ru>
2024-05-07 10:23:47 +0300

    more clear

--- Java-script №2.js
+++ Java-script №2.js
@@ -3044,3 +3044,10 @@ function tribonacci(signature,n){
   return resArr
 
 }
+function tribonacci2(signature,n) {
+  const result = signature.slice(0, n);
+  while (result.length < n) {
+    result[result.length] = result.slice(-3).reduce((p,c) => p + c, 0);
+  }
+  return result;
+}

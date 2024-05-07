09b641d74e8a21fa18eff923f80da44c258985d1
Gennadij Lobotskij <utyf185@mail.ru>
2024-05-07 09:47:15 +0300

    More clear

--- Java-script №2.js
+++ Java-script №2.js
@@ -3018,3 +3018,7 @@ var findMissing = function (l) {
     }
   }
 }
+var findMissing1 = function (list) {
+  var step = (list[list.length - 1] - list[0]) / (list.length);
+  return list.filter(function(val, index) { return val !== (list[0] + index * step); })[0] - step;
+}

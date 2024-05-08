29289dec786a23da2cbbba66885d232968bf1c4a
Gennadij Lobotskij <utyf185@mail.ru>
2024-05-08 19:13:21 +0300

    More clear

--- Java-script №2.js
+++ Java-script №2.js
@@ -3078,3 +3078,7 @@ function high(x){
   })
   return arr[resArr.indexOf(Math.max(...resArr))]
 }
+function high(s){
+  let as = s.split(' ').map(s=>[...s].reduce((a,b)=>a+b.charCodeAt(0)-96,0));
+  return s.split(' ')[as.indexOf(Math.max(...as))];
+}

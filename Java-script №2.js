6460d62e6435ed3586be64d470e2ab39cb706a58
Gennadij Lobotskij <utyf185@mail.ru>
2024-05-09 10:24:25 +0300

    more clear

--- Java-script №2.js
+++ Java-script №2.js
@@ -3115,3 +3115,10 @@ function twoSum(numbers, target) {
 
   return resArr
 }
+function twoSum(numbers, target) {
+  for (var i = 0; i < numbers.length-1; i++) {
+    for (var j = i+1; j < numbers.length; j++) {
+      if (numbers[i] + numbers[j] === target) return [i, j];
+    }
+  }
+}

49cfbd9404f9109644a343ab7414ce352ca19ceb
Gennadij Lobotskij <utyf185@mail.ru>
2024-05-15 19:51:31 +0300

    more clear

--- Java-script №2.js
+++ Java-script №2.js
@@ -3295,3 +3295,5 @@ function highestRank(arr){
   }
   return Number(highestNum)
 }
+const highestRank2 = arr =>
+    arr.sort((a, b) => arr.filter(val => val === b).length - arr.filter(val => val === a).length || b - a)[0];

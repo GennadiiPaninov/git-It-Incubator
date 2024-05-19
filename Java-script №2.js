431669c0a295fd87d0d9abe347403ee703b7bb2d
Gennadij Lobotskij <utyf185@mail.ru>
2024-05-19 17:41:24 +0300

    more clear

--- Java-script №2.js
+++ Java-script №2.js
@@ -3428,3 +3428,8 @@ function isAgeDiverse(list) {
   })
   return arr.every(e=> e===0)
 }
+function isAgeDiverse(list) {
+  return list.some(h => h.age >= 100)
+      ? [10,20,30,40,50,60,70,80,90].every(e => list.some(x => x.age - e >= 0 && x.age - e <= 9))
+      : false;
+}

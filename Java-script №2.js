aa4c799e29111d0ef6eb7d6796a2fba3eb862829
Gennadij Lobotskij <utyf185@mail.ru>
2024-05-20 19:41:42 +0300

    Is a number prime?

--- Java-script №2.js
+++ Java-script №2.js
@@ -3473,3 +3473,14 @@ function askForMissingDetails(l) {
     return 1
   }).filter(e=>e!==1)
 }
+
+function isPrime(n) {
+  if (n <= 1) return false
+  for (let i = 2; i <= Math.sqrt(n); i++) {
+    if (n % i === 0) {
+      return false
+    }
+  }
+
+  return true
+}

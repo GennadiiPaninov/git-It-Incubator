2f8d57ffdba8285e2c222aa1fd4258c3d982c638
Gennadij Lobotskij <utyf185@mail.ru>
2024-05-31 19:04:39 +0300

    Rectangle into Squares

--- Java-script №2.js
+++ Java-script №2.js
@@ -3754,3 +3754,12 @@ const findPairs = (nums, target) => {
     }
     return count
   }
+  function sqInRect(lng, wdth){
+    let arr = []
+    if(lng === wdth) return null
+    while(lng > 0 && wdth > 0){
+      arr.push(lng > wdth ? wdth : lng)
+      lng > wdth ? lng -= wdth : wdth -= lng
+    }
+    return arr
+  }

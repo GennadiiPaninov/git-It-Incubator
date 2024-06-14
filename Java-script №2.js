1d41c70631c2e2a25a0b6a297b11f83d5be53f64
Gennadij Lobotskij <utyf185@mail.ru>
2024-06-14 18:37:42 +0300

    Reducing by steps

--- Java-script №2.js
+++ Java-script №2.js
@@ -4021,3 +4021,13 @@ const findPairs = (nums, target) => {
 
     return Math.pow(2, set_.size) - 1;
   }
+  const gcdi = (a, b) => b ? gcdi(b, a % b) : Math.abs(a);
+  const lcmu = (a, b) => Math.abs(a * b) / gcdi(a, b);
+  const som  = (a, b) => a + b;
+  const maxi = (a, b) => Math.max(a, b);
+  const mini = (a, b) => Math.min(a, b);
+
+  const operArray = (fct, arr, init) => arr.map(u => {
+    init = fct(init, u);
+    return init;
+  });

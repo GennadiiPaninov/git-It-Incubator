7e80f4800b637435cf49815873f73cf36b3337e3
Gennadij Lobotskij <utyf185@mail.ru>
2024-06-14 19:04:25 +0300

    nore clear

--- Java-script №2.js
+++ Java-script №2.js
@@ -4043,3 +4043,7 @@ const findPairs = (nums, target) => {
     }
     return resArr
   }
+  function autocomplete(input, dictionary){
+    var r = new RegExp('^' + input.replace(/[^a-z]/gi,''), 'i');
+    return dictionary.filter(function(w){ return r.test(w); }).slice(0, 5);
+  }

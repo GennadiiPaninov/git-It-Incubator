7493eff2d3286980039a75636cc218c2c0374af7
Gennadij Lobotskij <utyf185@mail.ru>
2024-06-15 13:29:39 +0300

    more clear

--- Java-script №2.js
+++ Java-script №2.js
@@ -4073,3 +4073,7 @@ const findPairs = (nums, target) => {
     }
     return result;
   }
+  function deleteDigit(n) {
+    const s = String(n)
+    return Math.max(...Array.from(s, (_, i) => s.slice(0, i) + s.slice(i + 1)))
+  }

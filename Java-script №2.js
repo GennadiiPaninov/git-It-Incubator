37dc08346080aa8edff22156fe4399ed0f960433
Gennadij Lobotskij <utyf185@mail.ru>
2024-06-22 12:42:53 +0300

    Nothing special

--- Java-script №2.js
+++ Java-script №2.js
@@ -4229,3 +4229,7 @@ const findPairs = (nums, target) => {
   };
   const lengthOfSequence = (arr, n) =>
       arr.filter(val => val === n).length === 2 ? arr.lastIndexOf(n) - arr.indexOf(n) + 1 : 0;
+  function nothingSpecial(str) {
+    if(typeof str !== 'string') return 'Not a string!'
+    return str.replace(/[^A-Za-z0-9\s]/g, '')
+  }

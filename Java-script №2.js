f01af81cf2f72f9f613a286f79c15533173803a7
Gennadij Lobotskij <utyf185@mail.ru>
2024-07-11 10:13:09 +0300

    Nore clear

--- Java-script №2.js
+++ Java-script №2.js
@@ -4523,3 +4523,11 @@ const findPairs = (nums, target) => {
     }
     return Math.pow(sumNum, 2) - sumSquaresNum
   }
+  function differenceOfSquares(x){
+    var sum = 0, squareSum = 0, i;
+    for (i =1 ; i <= x; i++) {
+      sum += i*i;
+      squareSum += i;
+    }
+    return squareSum*squareSum - sum;
+  }

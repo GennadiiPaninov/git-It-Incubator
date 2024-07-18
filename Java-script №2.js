10e3b0f7200e2719cdd3159076207ced5b339e7f
Gennadij Lobotskij <utyf185@mail.ru>
2024-07-18 11:53:32 +0300

    more clear

--- Java-script №2.js
+++ Java-script №2.js
@@ -4746,3 +4746,12 @@ function compareItems(a, b) {
     return String(a).localeCompare(String(b));
   }
 }
+function sort(items) {
+  const swap = (arr, i , j) => [arr[i], arr[j]] = [arr[j], arr[i]];
+  for (let i = 0; i < items.length ; i++) {
+    for(let j = 0 ; j < items.length - i - 1; j++) {
+      if (items[j] > items[j + 1]) swap(items, j, j + 1);
+    }
+  }
+  return items;
+}

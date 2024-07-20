b55a668a9a9445aad7f9c8a43d95d8b2a5ea8409
Gennadij Lobotskij <utyf185@mail.ru>
2024-07-20 17:06:37 +0300

    more claer

--- Java-script №2.js
+++ Java-script №2.js
@@ -4844,3 +4844,12 @@ function sort(items){
 
   return sortArray
 }
+function sort(items) {
+  const swap = (arr, i , j) => [arr[i], arr[j]] = [arr[j], arr[i]];
+  for (let i = 0; i < items.length ; i++) {
+    for(let j = 0 ; j < items.length - i - 1; j++) {
+      if (items[j] > items[j + 1]) swap(items, j, j + 1);
+    }
+  }
+  return items;
+}

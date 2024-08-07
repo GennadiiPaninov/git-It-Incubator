ec01e4add5bd885e40a3a304e634db85776dc91b
Gennadij Lobotskij <utyf185@mail.ru>
2024-08-07 10:25:15 +0300

    more clear

--- Java-script №2.js
+++ Java-script №2.js
@@ -5340,3 +5340,10 @@ function solve(s) {
   }
   return res
 }
+function solve1(s) {
+  let res = s.replace(/\[backspace\]\*([0-9][0-9]*)/g, (m, p1) => '[backspace]'.repeat(p1))
+  while (res.includes('[backspace]')){
+    res = res.replace(/[A-Za-z ]\[backspace\]|^\[backspace\]/, '')
+  }
+  return res
+}

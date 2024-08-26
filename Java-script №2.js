9608d4ae963070fae7642a4ad44af186704958a0
Gennadij Lobotskij <utyf185@mail.ru>
2024-08-26 11:02:26 +0300

    The best solution

--- Java-script №2.js
+++ Java-script №2.js
@@ -5832,3 +5832,11 @@ Array.prototype.sortReloaded = function(dir = 'asc') {
 
   return sortedArray
 };
+Array.prototype.sortReloaded = function(dir='asc') {
+  const functions = {
+    'asc': (a, b) => a - b,
+    'desc': (a, b) => b - a,
+  };
+  const sortFunction = functions[dir];
+  return !sortFunction ? false : this.slice().sort(sortFunction);
+}

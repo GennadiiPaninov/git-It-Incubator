e65efc826a40c06a3cafb85d4d34839ee9d4ed69
Gennadij Lobotskij <utyf185@mail.ru>
2024-09-03 09:34:26 +0300

    Wave Sorting

--- Java-script №2.js
+++ Java-script №2.js
@@ -5999,3 +5999,9 @@ function updateInventory2(curStock, newStock) {
   })
   return Object.keys(obj).sort().map(k => [obj[k], k])
 }
+function waveSort(arr) {
+  arr.sort((a, b) => a - b)
+  for (let i = 0; i < arr.length - 1; i += 2) {
+    [arr[i], arr[i + 1]] = [arr[i + 1], arr[i]]
+  }
+}

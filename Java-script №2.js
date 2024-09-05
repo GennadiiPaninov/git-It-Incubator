86e6baf305a74b9ec7345d6e68600cfba9f6edaa
Gennadij Lobotskij <utyf185@mail.ru>
2024-09-05 10:41:59 +0300

    More clear

--- Java-script №2.js
+++ Java-script №2.js
@@ -6038,3 +6038,7 @@ function common(a,b,c){
 
   return sum
 }
+const common = (a,b,c) => {
+  [a,b,c] = [a,b,c].map(el => el.reduce((sum, num) => (sum[num] = (sum[num] || 0) + 1, sum), {}));
+  return Object.keys(a).reduce((sum, key) => sum + Math.min(a[key]||0, b[key]||0, c[key]||0) * key, 0);
+}

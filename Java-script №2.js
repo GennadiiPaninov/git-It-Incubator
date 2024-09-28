d1329ac1bb7739e809f42158108473d7e9bde5bb
Gennadij Lobotskij <utyf185@mail.ru>
2024-09-28 11:49:46 +0300

    Ackermann Function

--- Java-script №2.js
+++ Java-script №2.js
@@ -6220,6 +6220,15 @@ function squaresNeeded(g){
 function squaresNeeded2(g){
   return Math.ceil(Math.log2(grains+1))
 }
-function divisibleByThree(str){
+function divisibleByThree2(str){
   return str.split("").reduce((acc,cur)=>Number(cur)+acc,0) % 3===0
+}
+Ackermann = function(m,n) {
+    if (m === 0) {
+        return n + 1;
+    } else if (m > 0 && n === 0) {
+        return Ackermann(m-1, 1);
+    } else if (m > 0 && n > 0) {
+        return Ackermann(m-1, Ackermann(m, n-1));
+    }
 }

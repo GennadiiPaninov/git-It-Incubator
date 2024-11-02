70a95fc4140dd848278158acd6ae4a71a2bdc19e
Gennadij Lobotskij <utyf185@mail.ru>
2024-11-02 12:31:04 +0300

    More clear

--- Java-script №2.js
+++ Java-script №2.js
@@ -6780,3 +6780,14 @@ function findSpaceship1(map = "") {
   }
   return [7,2]
 }
+const findSpaceship = (map = "") => {
+  map = map.split("\n").reverse()
+
+  for (let y = 0; y < map.length; y++) {
+    for (let x = 0; x < map[y].length; x++) {
+      if (map[y][x] === "X") return [x, y]
+    }
+  }
+
+  return "Spaceship lost forever."
+}

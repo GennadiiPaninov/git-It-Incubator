f12b367c24a94687dc2b792e3150a9def79afa65
Gennadij Lobotskij <utyf185@mail.ru>
2024-11-24 11:59:40 +0300

    More clear

--- Java-script №2.js
+++ Java-script №2.js
@@ -7022,3 +7022,20 @@ function NamedOne(first, last) {
     this.fullName
   }
 }
+function NamedOne2(first, last) {
+  this.firstName = first;
+  this.lastName = last;
+
+  Object.defineProperty(this, "fullName", {
+    set: function(value) {
+      var parts = value.split(" ");
+      if (parts.length === 2) {
+        this.firstName = parts[0];
+        this.lastName = parts[1];
+      }
+    },
+    get: function() {
+      return this.firstName + ' ' + this.lastName;
+    }
+  });
+}

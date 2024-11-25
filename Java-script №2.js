b7969866250018a0ca096f9990f93b2b9d2e3b4e
Gennadij Lobotskij <utyf185@mail.ru>
2024-11-25 16:22:11 +0300

    More clear

--- Java-script №2.js
+++ Java-script №2.js
@@ -7053,3 +7053,13 @@ function revrot1(str, sz) {
       .map((v) => (isEven(v) ? reverse(v) : rotate(v)))
       .join('');
 }
+function revrot2(str, sz) {
+  if(!sz||sz<0||sz>str.length)return '';
+  var arr=str.match(new RegExp('\\d{'+sz+'}','g'));
+  return arr.map(function(v){
+    if(v.toString().split('').reduce((s,v)=>s+v*1,0)%2===0){
+      return v.split('').reverse().join('');
+    }
+    else return v.slice(1)+v[0];
+  }).join('');
+}

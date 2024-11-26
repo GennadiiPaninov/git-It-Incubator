64e9d3eb0b05fbb117b7195270b33eb65513bade
Gennadij Lobotskij <utyf185@mail.ru>
2024-11-26 10:58:47 +0300

    Recursion 101

--- Java-script №2.js
+++ Java-script №2.js
@@ -7063,3 +7063,9 @@ function revrot2(str, sz) {
     else return v.slice(1)+v[0];
   }).join('');
 }
+function solve(a,b){
+  if(a==0 || b ==0) return [a,b]
+  if(a>=2*b) return solve(a-2*b,b)
+  if(b>=2*a) return solve(a,b-2*a)
+  return [a,b]
+}

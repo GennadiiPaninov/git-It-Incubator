8fe4f4c5206a0a46f92ff28ff2c5db0c455e435a
Gennadij Lobotskij <utyf185@mail.ru>
2024-12-19 19:48:42 +0300

    Alphabet war - airstrike - letters massacre

--- Java-script №2.js
+++ Java-script №2.js
@@ -7222,3 +7222,12 @@ function sevenAte9(str) {
 function sevenAte9(str){
   return str.replace(/79(?=7)/g, '7');
 }
+function alphabetWar(fight){
+  fight = fight.replace(/[a-z]\*[a-z]|[a-z]\*|\*[a-z]/g,"")
+  var l = 0;
+  var r = 0;
+  fight.split("").forEach(i=>"1sbpw".indexOf(i)>0?l+="1sbpw".indexOf(i):l)
+  fight.split("").forEach(j=>"1zdqm".indexOf(j)>0?r+="1zdqm".indexOf(j):r)
+  if(l==r){return "Let's fight again!"}
+  return l>r ? "Left side wins!" : "Right side wins!";
+}

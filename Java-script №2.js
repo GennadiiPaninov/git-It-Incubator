0fd877b6b70c02f9da69d98f90a7673f90a38c0b
Gennadij Lobotskij <utyf185@mail.ru>
2025-01-10 19:11:04 +0300

    More clear

--- Java-script №2.js
+++ Java-script №2.js
@@ -7466,3 +7466,17 @@ function solve1(sarr){
   const m = isNaN(Math.floor(maxTime/60)) ? "00" : `${maxTime - ( h * 60)}`.length == 1 ? `0${maxTime - ( h * 60)}` : maxTime - ( h * 60)
   return `${h}:${m}`
 }
+function solve2(arr){
+  var sortedAsMinutes = arr.map(x => timeToMinutes(x)).sort((a, b) => a - b);
+  sortedAsMinutes.push(sortedAsMinutes[0] + 60 * 24);
+  return minutesToTime(Math.max.apply(null, sortedAsMinutes.map((x, i, a) => { return i < a.length - 1 ? a[i + 1] - x : 0 })) - 1);
+}
+
+function timeToMinutes(time) {
+  var tokens = time.split(":");
+  return parseInt(tokens[0], 10) * 60 + parseInt(tokens[1], 10);
+}
+
+function minutesToTime(minutes) {
+  return ("" + Math.floor(minutes / 60)).padStart(2, "0") + ":" + ("" + minutes % 60).padStart(2, "0");
+}

74c8ec2b3b17ba8d483a9fdabf05294d8f93274a
Gennadij Lobotskij <utyf185@mail.ru>
2025-01-16 09:35:08 +0300

    Counting String Subsequences

--- Java-script №2.js
+++ Java-script №2.js
@@ -7547,3 +7547,22 @@ function ranking(people) {
   })
   return res
 }
+function countSubsequences(needle, haystack) {
+  const n = needle.length;
+  const m = haystack.length;
+  const dp = Array(n + 1).fill(0).map(() => Array(m + 1).fill(0));
+
+  for (let j = 0; j <= m; j++) {
+    dp[0][j] = 1
+  }
+  for (let i = 1; i <= n; i++) {
+    for (let j = 1; j <= m; j++) {
+      if (needle[i - 1] === haystack[j - 1]) {
+        dp[i][j] = (dp[i][j - 1] + dp[i - 1][j - 1]) % 100000000
+      } else {
+        dp[i][j] = dp[i][j - 1]
+      }
+    }
+  }
+  return dp[n][m] % 100000000
+}
